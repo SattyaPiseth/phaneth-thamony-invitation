c25d8de63af5eb3ff52bb4bfb0b6a9a5e7c6e518
Sattya Piseth <pisethsattya33@gmail.com>
2025-10-08 15:04:09 +0700

    Auto update: 2025-10-08 15:04:08

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const DEFAULT_BG = {
 
 const BG_BY_ROUTE = {
   "/": { src: "/videos/background.mp4", poster: "/images/cover-page/background.png", loop: true },
-  "/home": { src: "/videos/background.mp4", poster: "/images/cover-page/background.png", loop: false },
+  "/home": { src: "/videos/homepage-background.mp4", poster: "/images/cover-page/background.png", loop: true },
 };
 
 export default function App() {
