39ff0f5f656c40f46758c1cffa88b030d589562c
Sattya Piseth <pisethsattya33@gmail.com>
2025-10-09 19:38:04 +0700

    Auto update: 2025-10-09 19:38:04

--- src/components/SoftCard.jsx
+++ src/components/SoftCard.jsx
@@ -3,7 +3,7 @@ export default function SoftCard({ className = "", children }) {
     <div
     // p-5
       className={[
-        "rounded-xl p-2 sm:p-7 md:p-8 lg:mx-[14.5vh] xl:mx-[19.5vh] 2xl:mx-auto text-center",
+        "rounded-xl p-2 sm:p-7 md:p-8 lg:mx-[14.5vh] xl:mx-[20vh] 2xl:mx-auto text-center",
         "bg-white/15 backdrop-blur-[2px] border border-white/20 shadow-sm",
         "motion-safe:animate-[fade-up_700ms_ease-out_both] motion-safe:[animation-delay:120ms]",
         "space-y-8", // unified vertical rhythm sm:space-y-8 md:space-y-10
