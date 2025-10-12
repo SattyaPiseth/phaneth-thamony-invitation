3129e0e933d856474d860a9474db407ce69a0cc1
Sattya Piseth <pisethsattya33@gmail.com>
2025-10-12 13:56:59 +0700

    Auto update: 2025-10-12 13:56:59

--- src/data/galleryImages.js
+++ src/data/galleryImages.js
@@ -3,7 +3,6 @@ export const GALLERY_IMAGES = [
   { src: "/images/home-page/gallary/landscape-01.jpg", width: 1800, height: 1200 },
   { src: "/images/home-page/gallary/landscape-02.jpg", width: 1800, height: 1200 },
   { src: "/images/home-page/gallary/landscape-03.jpg", width: 1800, height: 1200 },
-  { src: "/images/home-page/gallary/portrait-03.jpg",  width: 1200, height: 1600 },
   { src: "/images/home-page/gallary/landscape-04.jpg", width: 1800, height: 1200 },
   { src: "/images/home-page/gallary/portrait-04.jpg",  width: 1200, height: 1600 },
   { src: "/images/home-page/gallary/portrait-05.jpg",  width: 1200, height: 1600 },
