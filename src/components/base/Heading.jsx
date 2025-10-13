b1522c306f81c80d8ec4195257354f522c61f215
Sattya Piseth <pisethsattya33@gmail.com>
2025-10-13 16:07:16 +0700

    Auto update: 2025-10-13 16:07:16

--- src/components/base/Heading.jsx
+++ src/components/base/Heading.jsx
@@ -24,7 +24,6 @@ export default function Heading({
       {!isStoryPlaying && (
         <img
           src={src}
-          fetchPriority="high"
           alt="Cover page heading"
           width={414}
           height={207}
