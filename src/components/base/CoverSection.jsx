78f9e514d36752fb6f4225648c0f21848f4c8249
Sattya Piseth <pisethsattya33@gmail.com>
2025-10-14 23:20:08 +0700

    Auto update: 2025-10-14 23:20:08

--- src/components/base/CoverSection.jsx
+++ src/components/base/CoverSection.jsx
@@ -58,7 +58,7 @@ export default function CoverSection({
               className={cn(
                 
                 "moul-regular text-center tracking-wide leading-6",
-                "text-sm text-wrap px-[15vw]",
+                "text-sm text-wrap px-[20vw]",
                 "text-[var(--accent)]"
               )}
             >
